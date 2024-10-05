f0ef99b006e133fd06938a5fda70837820df8e93
Telegin Oleg <teleginos@mail.ru>
2024-10-05 18:24:10 +0300

    Initial commit

---
+++ app/models/user.py
@@ -0,0 +1,20 @@
+from app.backend.db import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+from sqlalchemy.orm import relationship
+Base.metadata.clear()
+class User(Base):
+    __tablename__ = "users"
+    __table_args_ = {"extend_existing": True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+
+    tasks = relationship("Task", back_populates="user")
+
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(User.__table__))
